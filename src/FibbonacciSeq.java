03ba44b95b511f5a31b20a1be792a7b0aae04e2e
umigit16 <uiskand2@gmail.com>
2022-11-12 21:05:08 -0600

    Initial commit

---
+++ src/FibbonacciSeq.java
@@ -0,0 +1,24 @@
+public class FibbonacciSeq {
+
+    static void FibbonociNums(int N) {
+
+        int num1 = 0, num2 = 1;
+
+        int count = 0;
+
+        while (count < N ) {
+            System.out.print(num1  + ", ");
+            int num3 = num1 + num2;
+            num1 = num2;
+            num2 = num3;
+            count ++;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        FibbonociNums(5);
+    }
+
+
+}
